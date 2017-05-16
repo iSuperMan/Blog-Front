2a3d0fc5fdcc802094903824531e9027ccf13459
Andrei <horoshavin-andr@mail.ru>
2017-05-17 00:30:11 +0300

    Add main link to logo-title

--- app/components/Header/components/Title/index.jsx
+++ app/components/Header/components/Title/index.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PersonIcon from 'material-ui/svg-icons/social/person';
 import type { User } from '../../../../services/entities/user';
 import styles from './assets/styles.css';
@@ -9,7 +10,9 @@ type TitleProps = {
 };
 
 const Title = (props: TitleProps) => <div className={styles.title}>
-  <x-title>Blog</x-title>
+  <Link to="/">
+		<x-title>Blog</x-title>
+	</Link>
 
 	{props.user && <x-user-info>
 		<x-user-icon>
